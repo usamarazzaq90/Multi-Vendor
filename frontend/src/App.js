051cecac0f57c0c0bdd24c301d6dc1f2c13a6f26
usamarazzaq90 <usamarazzaq9014@gmail.com>
2025-06-08 23:13:49 +0500

    Completed the frontend of the login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage } from "./Routes.js";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
